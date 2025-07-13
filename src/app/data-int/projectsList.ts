baf17d22cdb1ad1d976063340f67ad70a74a6d13
BluTargaryan <opeyemiyolo@gmail.com>
2025-07-13 23:48:55 +0100

    project list update

--- src/app/data-int/projectsList.ts
+++ src/app/data-int/projectsList.ts
@@ -1,11 +1,17 @@
 export const projectsList = () =>{
     return[
+        // {
+        //   name:'Ignite',
+        //   img:'https://images.unsplash.com/photo-1676929197571-451fd22ba431?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        //   github:'https://github.com/BluTargaryan/Ignite',
+        //   site:'https://super-donut-699c99.netlify.app/'
+        // },
         {
-          name:'Ignite',
-          img:'https://images.unsplash.com/photo-1676929197571-451fd22ba431?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-          github:'https://github.com/BluTargaryan/Ignite',
-          site:'https://super-donut-699c99.netlify.app/'
-        },
+            name:'Statify',
+            img:'https://images.unsplash.com/photo-1714583173985-fa58ef40c8d4?q=80&w=1472&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            github:'https://github.com/BluTargaryan/statify-mma-prototype',
+            site:'https://statify-mma-prototype-pd4q.vercel.app/'
+          },
         {
             name:'Houzen',
             img:'https://images.unsplash.com/photo-1516156008625-3a9d6067fab5?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
@@ -18,12 +24,18 @@ export const projectsList = () =>{
             github:'https://github.com/BluTargaryan/Dimensional',
             site:'https://dashing-begonia-2e335d.netlify.app/'
           },
+          // {
+          //   name:'Bookstore',
+          //   img:'https://images.unsplash.com/photo-1640534936843-b2c07f810bd6?q=80&w=1771&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+          //   github:'https://github.com/BluTargaryan/Bookstore-MERN',
+          //   site:'https://main.d2n68njuf58ecj.amplifyapp.com/'
+          // },
           {
-            name:'Bookstore',
-            img:'https://images.unsplash.com/photo-1640534936843-b2c07f810bd6?q=80&w=1771&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-            github:'https://github.com/BluTargaryan/Bookstore-MERN',
-            site:'https://main.d2n68njuf58ecj.amplifyapp.com/'
-          },
+            name:'3D Showcase',
+            img:'https://images.unsplash.com/photo-1640906152676-dace6710d24b?q=80&w=1332&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            github:'https://github.com/BluTargaryan/threefiber-udemy-2025-learn',
+            site:'https://threefiber-udemy-2025-learn.vercel.app/'
+          }
     ]
 }
 

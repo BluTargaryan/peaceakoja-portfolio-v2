09285bb84f1b06d319a8c7164f31fbcca4174d50
BluTargaryan <opeyemiyolo@gmail.com>
2026-04-01 16:35:57 +0100

    fixed works data issue, collating data for history section

---
+++ src/app/history/page.tsx
@@ -0,0 +1,22 @@
+
+
+const History = () => {
+
+
+  return (
+    <main className="flex flex-col items-center h-full w-full py-23 overflow-y-scroll">
+
+<section className="flex flex-col items-center text-center w-[260px] mb-12 gap-4">
+<h1>History</h1>
+<p>My education and experience as a frontend engineer.</p>
+</section>
+
+
+
+
+
+    </main>
+  )
+}
+
+export default History
